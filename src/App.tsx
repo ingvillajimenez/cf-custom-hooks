17385731597a8e6c0761b93f9f837b41a6fbecfe
Juan Villa <ing.villa.jimenez@gmail.com>
2025-01-17 11:56:35 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import ClickOutside from "./components/ClickOutside";
+import ForceUpdate from "./components/ForceUpdate";
+import Toggle from "./components/Toggle";
+import LocalStorage from "./components/LocalStorage";
+import IsOnline from "./components/IsOnline";
+import Previous from "./components/Previous";
+import Interval from "./components/Interval";
+
+function App() {
+  return (
+    <main>
+      <ClickOutside />
+      <ForceUpdate />
+      <Toggle />
+      <LocalStorage />
+      <IsOnline />
+      <Previous />
+      <Interval />
+    </main>
+  );
+}
+
+export default App;
